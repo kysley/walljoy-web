c430617a7b40dd34944dd528785d8e616c6fdc7e
Evan Kysley <evan@kysley.com>
2021-07-12 09:41:17 -0400

    add authExchange

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-import {createClient} from 'urql';
+import {createClient, dedupExchange, cacheExchange, fetchExchange} from 'urql';
+import {authExchange} from '@urql/exchange-auth';
+import {makeOperation} from '@urql/core';
 
 const clientUrl =
   import.meta.env.mode === 'PRODUCTION'
@@ -18,6 +20,83 @@ export const urqlClient = createClient({
       },
     };
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange,
+    authExchange({
+      addAuthToOperation: ({authState, operation}) => {
+        if (!authState || !authState.token) {
+          return operation;
+        }
+        const fetchOptions =
+          typeof operation.context.fetchOptions === 'function'
+            ? operation.context.fetchOptions()
+            : operation.context.fetchOptions || {};
+
+        return makeOperation(operation.kind, operation, {
+          ...operation.context,
+          fetchOptions: {
+            headers: {
+              ...fetchOptions.headers,
+              // Authorization: authState.token,
+            },
+          },
+        });
+      },
+      willAuthError: ({authState}) => {
+        if (!authState) return true;
+        // e.g. check for expiration, existence of auth etc
+        return false;
+      },
+      didAuthError: ({error}) => {
+        // check if the error was an auth error (this can be implemented in various ways, e.g. 401 or a special error code)
+        console.log(
+          error,
+          error.graphQLErrors.some((e) =>
+            e.message.toLowerCase().includes('expired'),
+          ),
+        );
+        return error.graphQLErrors.some((e) =>
+          e.message.toLowerCase().includes('expired'),
+        );
+      },
+      getAuth: async ({authState, mutate}) => {
+        if (!authState) {
+          const token = localStorage.getItem('token');
+          const refreshToken = localStorage.getItem('refreshToken');
+          if (token && refreshToken) {
+            return {token, refreshToken};
+          }
+          return null;
+        }
+
+        const result = await mutate(
+          `mutation RefreshCredentials($refreshToken: String!) {
+  refreshCredentials(refreshToken: $refreshToken) {
+    token
+    refreshToken
+  }
+}`,
+          {
+            refreshToken: authState!.refreshToken,
+          },
+        );
+
+        if (result?.data.refreshCredentials) {
+          const {token, refreshToken} = result?.data.refreshCredentials;
+          localStorage.setItem('token', token);
+          localStorage.setItem('refreshToken', refreshToken);
+          return {
+            token: token,
+            refreshToken: refreshToken,
+          };
+        }
+        localStorage.clear();
+        return null;
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 export const saveTokens = (token: string, refreshToken: string) => {
